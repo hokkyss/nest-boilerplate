34c350e05d8923a8cc3263239e1d5a0df671ecbf
Hokki Suwanda <hokki.suwanda@nri.co.id>
2023-06-06 19:02:56 +0700

    feat: add prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { JoiPipeModule } from 'nestjs-joi';
 import { HelloWorldModule } from './modules/hello-world/hello-world.module';
 
 @Module({
-  imports: [JoiPipeModule, HelloWorldModule],
+  imports: [
+    JoiPipeModule,
+    HelloWorldModule,
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+      isGlobal: true,
+    }),
+    // { module: PrismaModule, global: true },
+  ],
   providers: [
     {
       provide: APP_INTERCEPTOR,
