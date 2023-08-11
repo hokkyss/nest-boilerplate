4094846ac99e61d54b97d0d562eac9c6769d0016
Hokki Suwanda <hokkyss2@gmail.com>
2023-08-12 00:30:03 +0700

    fix: eslint

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,8 +1,12 @@
-import AppModule from '@/app.module';
-import { VersioningType, type INestApplication } from '@nestjs/common';
+import type { INestApplication } from '@nestjs/common';
+import type { TestingModule } from '@nestjs/testing';
+
+import { VersioningType } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { Test, type TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import request from 'supertest';
+
+import AppModule from '@/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
